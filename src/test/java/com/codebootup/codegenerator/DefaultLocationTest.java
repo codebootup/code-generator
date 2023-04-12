ae05c90c87fa6a8e00f4b3388b507907e99f140f
Lee Cooper <lee@codebootup.com>
2023-04-13 06:17:34 +0800

    Initial

---
+++ src/test/java/com/codebootup/codegenerator/DefaultLocationTest.java
@@ -0,0 +1,12 @@
+package com.codebootup.codegenerator;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class DefaultLocationTest {
+    @Test
+    public void checkDefaultLocationIsAsExpected(){
+        Assertions.assertThat(new DefaultLocation().getBaseDirectory()).isEqualTo("");
+        Assertions.assertThat(new DefaultLocation().getFileDirectory()).isEqualTo("");
+    }
+}
