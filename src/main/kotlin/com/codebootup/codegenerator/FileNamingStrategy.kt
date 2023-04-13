73d6e61c13c9f9130babf54a4f78be43b91b6b84
codebootuphong <hong@codebootup.com>
2023-04-13 20:15:07 +0800

    Improved readability of tests by referring to class names and added some kdocs

--- src/main/kotlin/com/codebootup/codegenerator/FileNamingStrategy.kt
+++ src/main/kotlin/com/codebootup/codegenerator/FileNamingStrategy.kt
@@ -21,42 +21,79 @@ interface FileNamingStrategy {
     fun name(model: TemplateModel): String
 }
 
-class ItemInFocusFileNamingStrategy(private val path: String, private val suffix: String) : FileNamingStrategy {
+/**
+ * Strategy for naming a file using an item in the list
+ *
+ * @param pathExpression The path expression in Spring Expression Language (SPEL)
+ * @param suffix The file extension
+ */
+class ItemInFocusFileNamingStrategy(
+    private val pathExpression: String,
+    private val suffix: String
+) : FileNamingStrategy {
     override fun name(model: TemplateModel): String = ExpressionParserFileNamingStrategy(
-        path = path,
+        pathExpression = pathExpression,
         suffix = suffix,
         model = model.itemInFocus,
     ).parse()
 }
 
-class ModelInFocusFileNamingStrategy(private val path: String, private val suffix: String) : FileNamingStrategy {
-
+/**
+ * Strategy for naming a file using the model in focus
+ *
+ * @param pathExpression The path expression in Spring Expression Language (SPEL)
+ * @param suffix The file extension
+ */
+class ModelInFocusFileNamingStrategy(
+    private val pathExpression: String,
+    private val suffix: String
+) : FileNamingStrategy {
     override fun name(model: TemplateModel): String = ExpressionParserFileNamingStrategy(
-        path = path,
+        pathExpression = pathExpression,
         suffix = suffix,
         model = model.modelInFocus,
     ).parse()
 }
 
-class RootFileNamingStrategy(private val path: String, private val suffix: String) : FileNamingStrategy {
-
+/**
+ * Strategy for naming a file using the root model
+ *
+ * @param pathExpression The path expression in Spring Expression Language (SPEL)
+ * @param suffix The file extension
+ */
+class RootFileNamingStrategy(
+    private val pathExpression: String,
+    private val suffix: String
+) : FileNamingStrategy {
     override fun name(model: TemplateModel): String = ExpressionParserFileNamingStrategy(
-        path = path,
+        pathExpression = pathExpression,
         suffix = suffix,
         model = model.root,
     ).parse()
 }
 
+/**
+ * Strategy for naming a file with a hard-coded value
+ *
+ * @param filename Name of the file
+ */
 class SimpleFileNamingStrategy(private val filename: String) : FileNamingStrategy {
     override fun name(model: TemplateModel): String = filename
 }
 
+/**
+ * Strategy to parse an object using SPEL for generating the file name
+ *
+ * @param model The object in question used in parsing the path expression
+ * @param pathExpression The path expression in Spring Expression Language (SPEL)
+ * @param suffix The file extension
+ */
 private class ExpressionParserFileNamingStrategy(
     private val model: Any,
-    private val path: String,
+    private val pathExpression: String,
     private val suffix: String,
 ) {
     fun parse(): String {
-        return "${(SpelExpressionParserObject.parseExpression(path).getValue(model)?.toString() ?: throw CodeGeneratorParseException())}.$suffix"
+        return "${(SpelExpressionParserObject.parseExpression(pathExpression).getValue(model)?.toString() ?: throw CodeGeneratorParseException())}.$suffix"
     }
 }
